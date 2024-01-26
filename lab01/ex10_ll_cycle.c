e09b4e255dfd4c2c4aab4a1567b4a19d90b853ee
root <root@jkdai-win11.localdomain>
2024-01-26 20:20:22 +0800

    lab1

--- lab01/ex10_ll_cycle.c
+++ lab01/ex10_ll_cycle.c
@@ -3,4 +3,20 @@
 
 int ll_has_cycle(node *head) {
     /* TODO: Implement ll_has_cycle */
+    node* fast_ptr = head;
+    node* slow_ptr = head;
+    while (1)
+    {
+        if (fast_ptr == NULL) return 0;
+        fast_ptr = fast_ptr->next;
+        if (fast_ptr == NULL) return 0;
+        fast_ptr = fast_ptr->next;
+        if (fast_ptr == NULL) return 0;
+        slow_ptr = slow_ptr->next;
+        if (fast_ptr == slow_ptr)
+        {
+            return 1;
+        }
+    }
+    
 }
